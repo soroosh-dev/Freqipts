689654654ed658eb63143a840bbc4ec1a9cc6c2b
Soroosh <soroosh@tuta.io>
2025-09-02 14:15:16 +0330

    Removed installation of pip for python3.13 to resolve the error with distutils.

--- python_dev_setup.sh
+++ python_dev_setup.sh
@@ -20,10 +20,6 @@ apt update
 echo "Installing Python 3.13..."
 apt install -y python3.13 python3.13-venv python3.13-dev
 
-# Install pip for Python 3.13
-echo "Installing pip for Python 3.13..."
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.13
-
 # Create symlinks (optional, uncomment if you want to use python3.13 as default python3)
 # update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.13 1
 # update-alternatives --set python3 /usr/bin/python3.13
@@ -47,11 +43,6 @@ systemctl start nginx
 # Install additional development tools
 echo "Installing additional development tools..."
 apt install -y build-essential libssl-dev libffi-dev python3-dev python3-pip
-
-# Install common Python packages
-echo "Installing common Python packages..."
-python3.13 -m pip install --upgrade pip
-python3.13 -m pip install virtualenv virtualenvwrapper
 
 # Create a virtual environment (optional)
 # echo "Creating a virtual environment..."
